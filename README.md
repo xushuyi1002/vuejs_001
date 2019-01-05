016cf8d3f755666712d9ccf1ff1e03fc8d3c68fd
xushuyi <xsy123456>
2019-01-05 21:58:30 +0800

    提交文件

--- README.md
+++ README.md
@@ -27,3 +27,53 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### Xusy At 20190105 Start Studing Vuejs...
+1. 安装Vue
+```
+注：node版本必须大于或等于8.9版本
+   vue-cli3.x: npm install -g @vue/cli
+   vue-cli2.x: npm install -g @vue/cli-init
+```
+2. 创建项目 
+```
+vue init webpack my-project
+注：安装依赖的时候 选择最后一个，就是自己安装
+cd my-project
+npm start/npm run dev
+```
+3. 工程目录
+
+4. 基础指令
+```
+https://cn.vuejs.org/v2/guide/syntax.html
+1. Mustache：{{ 变量 }} 只能存在单行语句
+2. v-once：只能够渲染一次
+3. v-html：解析html结构
+4. v-bind：指令（解析属性中的对象）
+5. v-bind简写：（:）
+6. v-if：条件渲染
+7. v-show：条件渲染
+```
+5. v-if vs v-show
+```
+v-if 是“真正”的条件渲染，因为它会确保在切换过程中条件块内的事件监听器和子组件适当地被销毁和重建。
+v-if 也是惰性的：如果在初始渲染时条件为假，则什么也不做——直到条件第一次变为真时，才会开始渲染条件块。
+相比之下，v-show 就简单得多——不管初始条件是什么，元素总是会被渲染，并且只是简单地基于 CSS 进行切换。
+一般来说，v-if 有更高的切换开销，而 v-show 有更高的初始渲染开销。因此，如果需要非常频繁地切换，则使用 v-show 较好；如果在运行时条件很少改变，则使用 v-if 较好。
+```
+6. html 代码实现一些符号，大于号、小于号、商标等
+```
+ &lt;  	 < 	 小於号或显示标记    
+ &gt;	     >	     大於号或显示标记
+ &amp;     &	     可用於显示其它特殊字符  
+ &quot;	 "	     引号
+ &reg;	     ®	     己注册
+ &copy; 	 ©	     版权
+ &trade;	 ™	     商标
+ &ensp;	 	     半方大的空白
+ &emsp;	 	     全方大的空白
+ &nbsp;	 	     不断行的空白
+```
+
+
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,105 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+        {{ '你好么 Hello World!'}}
+        {{ 20 + 1}}
+        {{ 'ok'?'yes':'no'}}
+        {{ 'hello'.split('').reverse().join('')}}
+        <p>
+            <span v-once>这个将不会改变: {{ msg }}</span>
+        </p>
+        <div v-html="rawHtml"></div>
+        <!--<a href="http://baidu.com">{{urlName}}</a>-->
+        <a v-bind:href="url">{{urlName}}</a>&nbsp;&nbsp;
+        <a :href="url">{{urlName1}}</a>
+        <div>
+            <button v-bind:disabled="isDisabled">Button</button>
+        </div>
+        <div v-bind:id="dynamicId">动态的Id绑定.</div>
+        <div v-bind:id="'list-' + id">hh</div>
+        <a v-on:click="doSomething">跳转百度...</a> &nbsp;&nbsp;
+        <a @click="doSomething">跳转百度(v-on:click简写)...</a>
+
+        <p v-if="seen">现在你看到我了</p>
+        <p v-if="ifSwk">孙悟空</p>
+        <p v-else>通臂猿猴</p>
+
+        <!-- v-else 元素必须紧跟在带 v-if 或者 v-else-if 的元素的后面，否则它将不会被识别。 -->
+        <div v-if="randomVal>0.5">{{randomVal}} &gt; 0.5. Now you can see me.</div>
+        <div v-else>{{randomVal}} &lt; 0.5 .Now you don't see me.</div>
+
+        <p v-show="ifShow">英雄争霸</p>
+
+        <!-- 列表渲染-->
+        <ul>
+            <li v-for="item in itmes" v-bind:key="item.id">
+                {{item}}
+            </li>
+        </ul>
+
+        <ul>
+            <li v-for="(message, index) in messages" v-bind:key="index">
+                序号：{{index}} - 姓名：{{message.name}} - 年龄：{{message.age}}
+            </li>
+        </ul>
+
+        <ul>
+            <li v-for="(value, key,index) in obj" v-bind:key="index">
+                序号：{{index}} - Key值：{{key}} - Val值：{{value}}
+            </li>
+        </ul>
+
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    export default {
+        name: 'HelloWorld',
+        // props: {
+        //     msg: String
+        // },
+        data() {
+            return {
+                msg: "hello Vuejs.",
+                rawHtml: "<span style='color:red'>This should be red.</span>",
+                urlName: "淘宝",
+                url: "http://taobao.com",
+                isDisabled: false,
+                dynamicId: 12,
+                id: 123,
+                urlName1: "淘宝 - v-bind简写",
+                seen: true,
+                doSomething() {
+                    window.location.href = "http://baidu.com";
+                },
+                ifSwk: true,
+                randomVal: Math.random(),
+                ifShow: true,
+                itmes: ['java', 'js', 'mysql'],
+                messages: [
+                    {name: 'a', age: 20},
+                    {name: 'b', age: 21},
+                    {name: 'c', age: 22}
+                ],
+                obj: {
+                    name: "xushuyi",
+                    age: 20
+                }
+            }
+        }
+    }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+    ul {
+        margin: 0;
+        padding: 0
+    }
+
+    li {
+        margin-left: 650px;
+        text-align: left;
+    }
+
 </style>
